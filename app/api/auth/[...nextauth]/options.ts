25e9fd996ed484a5848fe1108762e29aa611c13d
Dinann <notxurd46@gmail.com>
2024-07-23 00:52:12 +0700

    Intial Commit

---
+++ app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,48 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      id: "credentials",
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        const user = { id: "1", name: "J Smith", email: "jsmith@example.com" };
+
+        if (user) {
+          return user;
+        } else {
+          return null;
+        }
+      },
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID as string,
+      clientSecret: process.env.GOOGLE_SECRET as string,
+    }),
+  ],
+  callbacks: {
+    async signIn({ user, account }) {
+      const isAllowedToSignIn = true;
+      if (isAllowedToSignIn) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    async redirect({ baseUrl }: { baseUrl: string }) {
+      return `${baseUrl}/`;
+    },
+    async session({ session, token }) {
+      return session;
+    },
+  },
+  pages: {
+    signIn: "/",
+  },
+};
