7d4e8a4da7a72147e9b246d606399fc6e6f09c2c
Dinann <notxurd46@gmail.com>
2024-07-28 22:25:05 +0700

    feat(card): added StatsCard for dashboard

--- app/(root)/dashboard/page.tsx
+++ app/(root)/dashboard/page.tsx
@@ -1,4 +1,6 @@
+import StatsCard from "@/components/cards/StatsCard";
 import RegisterForm from "@/components/forms/RegisterForm";
+import { Blocks, Container, Package, Receipt } from "lucide-react";
 import { Metadata } from "next";
 import React from "react";
 
@@ -6,8 +8,50 @@ export const metadata: Metadata = {
   title: "Dashboard",
 };
 
+const DUMMY_DATA = [
+  {
+    title: "Total Products",
+    amount: 12,
+    color: "255, 203, 17",
+    icon: <Package size={28} />,
+  },
+  {
+    title: "Total Inventories",
+    amount: 3,
+    color: "17, 141, 255",
+    icon: <Container size={28} />,
+  },
+  {
+    title: "Total Categories",
+    amount: 7,
+    color: "136, 17, 255",
+    icon: <Blocks size={28} />,
+  },
+  {
+    title: "Total Transactions",
+    amount: 24,
+    color: "255, 146, 17",
+    icon: <Receipt size={28} />,
+  },
+];
+
 function DashboardPage() {
-  return <div>Dashboard</div>;
+  return (
+    <div>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-4">
+        {DUMMY_DATA.map((card) => (
+          <div key={card.title}>
+            <StatsCard
+              title={card.title}
+              amount={card.amount}
+              color={card.color}
+              icon={card.icon}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default DashboardPage;
---
+++ components/cards/StatsCard.tsx
@@ -0,0 +1,34 @@
+type StatsCardProps = {
+  title: string;
+  amount: number;
+  color: string;
+  icon?: React.ReactNode;
+};
+
+function StatsCard({ title, amount, color, icon }: StatsCardProps) {
+  return (
+    <article className="rounded-md bg-accent/80 px-4 py-6 md:px-6 md:py-8">
+      <div className="flex items-center justify-between">
+        <div className="space-y-3">
+          <span className="text-xs font-medium uppercase text-desc">
+            {title}
+          </span>
+          <p className="text-lg font-semibold tracking-wide lg:text-xl">
+            {amount} {title.split(" ")[1]}
+          </p>
+        </div>
+        <div
+          className="rounded-full p-4"
+          style={{
+            color: `rgb(${color})`,
+            backgroundColor: `rgba(${color}, 0.2)`,
+          }}
+        >
+          {icon}
+        </div>
+      </div>
+    </article>
+  );
+}
+
+export default StatsCard;
